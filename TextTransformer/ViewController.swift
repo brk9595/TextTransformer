be315f27fd1721c0b31436f2e5bbb667fc23a727
bharathb9595@gmail.com <bharathb9595@gmail.com>
2019-04-14 22:40:34 +0530

    Initial Commit

---
+++ TextTransformer/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  TextTransformer
+//
+//  Created by Bharath  Raj kumar on 14/04/19.
+//  Copyright © 2019 Bharath Raj Kumar. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
